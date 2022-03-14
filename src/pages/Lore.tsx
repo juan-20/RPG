bd006c18fd0b8fbf052b1a836cc1bdb2b1825358
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2022-03-14 19:48:05 -0300

    fixing bugs 🚨

--- src/pages/Lore.tsx
+++ src/pages/Lore.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import NavBar from '../component/navBar';
-
-// import { Container } from './styles';
-
 const Lore: React.FC = () => {
     return (
         <>
--- src/pages/Mundo.tsx
+++ src/pages/Mundo.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from '../component/navBar';
 
 // import { Container } from './styles';
 
