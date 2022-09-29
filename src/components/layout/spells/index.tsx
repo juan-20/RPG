8cd6932bf5c925b953664cf707fbb6e7cee49218
juan-20 <juanalvesandrade@gmail.com>
2022-09-28 23:13:36 -0300

    🗃️

--- src/components/layout/spells/index.tsx
+++ src/components/layout/spells/index.tsx
@@ -15,12 +15,12 @@ export default function Speels(props: {spells: SpellsType}  ) {
       <Heading fontSize='4xl'>{props.spells.name}</Heading>  
       
         <div className="subHead">
-          <Image src={props.spells.distance.url} width='20' height='20' />
+          {/* <Image src={props.spells.distance.url} width='20' height='20' />
           <p>{props.spells.distance.quantity}</p>
           <Image src={props.spells.duration.url} width='20' height='20' />
           <p>{props.spells.duration.quantity}</p>
           <Image src={props.spells.type.url} width='20' height='20' />
-          <p>{props.spells.type.quantity}</p>
+          <p>{props.spells.type.quantity}</p> */}
         </div>
       </header>
       <p>{props.spells.description}</p>
