123a8695c3ca688d0e4a228d20d621f2ef33589e
juan-20 <juanalvesandrade@gmail.com>
2022-10-03 18:50:41 -0300

    Fixing TS

--- src/pages/api/Characters/[characterName].ts
+++ src/pages/api/Characters/[characterName].ts
@@ -1,15 +1,15 @@
 
+import { NextApiRequest, NextApiResponse } from 'next/types';
 import { characters } from '../data/character';
 
 
 
 export default function handler(
-    req,
-    res
+  req: NextApiRequest,
+  res: NextApiResponse<any>
   ) {
     const { characterName } = req.query
     if (req.method === 'GET') {
-
         
         const test = characters.find(character => character.name === characterName)
 
