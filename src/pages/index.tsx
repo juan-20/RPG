e9da002910ae81de13bc708e655c7e3b8eb40ee1
juan-20 <juanalvesandrade@gmail.com>
2022-10-26 22:39:02 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,27 @@
-export const getStaticProps = async () => {
-  let url = process.env.ENVIROMENT
-   const res = await fetch(url + '/api/Characters')
-   const characters: CharactersType[] = await res.json()
-   return{
-     props:{
-      characters,
-      fallback: false
-     }
-   }
+// export const getStaticProps = async () => {
+//   let url = process.env.ENVIROMENT
+//    const res = await fetch(url + '/api/Characters')
+//    const characters: CharactersType[] = await res.json()
+//    return{
+//      props:{
+//       characters,
+//       fallback: false
+//      }
+//    }
    
- }
+//  }
 
 import type { InferGetStaticPropsType } from 'next'
 import CharacterResume from '../components/layout/Character-resume'
 import { Body } from '../components/layout/Character-resume/styles'
 import Landing from '../components/layout/Landing'
 import { CharactersType } from '../types/D&D.type'
+import { characters } from './api/data/character'
 
-export default function Home({characters}: InferGetStaticPropsType<typeof getStaticProps>) {
+// {characters}: InferGetStaticPropsType<typeof getStaticProps>
+
+export default function Home() {
+  
   return (
     <>
     {characters.length > 0 ?
