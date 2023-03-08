0f84eed06aa9ac2ceb26a43c558d5f6268958686
juan-20 <juanalvesandrade@gmail.com>
2023-03-08 18:12:22 -0300

    adding SSR to spellname page and OG

--- src/pages/spells/[spellName].tsx
+++ src/pages/spells/[spellName].tsx
@@ -9,22 +9,25 @@ import { db } from '../../services/firebase';
 import { useRouter } from "next/router";
 import Link from 'next/link';
 import { CircleNotch } from 'phosphor-react';
+import { GetServerSideProps, GetStaticPaths } from 'next/types';
 
 export default function SpellsName({AllSpells}: any) {
   const router = useRouter();
   const { spellName }: any = router.query;
-  console.log(spellName);
-  const [allSpellLoaded, setallSpellSLoaded] = useState<SpellsProps[]>(AllSpells)
 
-  useEffect(() => {
-    if(spellName === undefined)return
-    fetch(`https://rpg-73e66-default-rtdb.firebaseio.com/spells.json?orderBy="SpellName"&equalTo="${spellName}"`)
-    .then(res => res.json())
-    .then(data => {
-      setallSpellSLoaded(Object.values(data))
-      console.log(allSpellLoaded)
-    })
-  }, [spellName])
+  const [allSpellLoaded, setallSpellSLoaded] = useState<SpellsProps[]>(Object.values(AllSpells))
+  console.log(AllSpells);
+  console.log(allSpellLoaded);
+
+  // useEffect(() => {
+  //   if(spellName === undefined)return
+  //   fetch(`https://rpg-73e66-default-rtdb.firebaseio.com/spells.json?orderBy="SpellName"&equalTo="${spellName}"`)
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     setallSpellSLoaded(Object.values(data))
+  //     console.log(allSpellLoaded)
+  //   })
+  // }, [spellName])
 
 
   return (
@@ -35,8 +38,8 @@ export default function SpellsName({AllSpells}: any) {
             <meta property='og:title' content='Magias D&D' />
             <meta property='og:description' content='Lista de todas magias de D&D 5e' />
             <meta property='og:type' content='website' />
-            <meta property="og:image" content={`${process.env.REACT_APP_ENVIROMENT}/api/og?title=$Magia&image=wand`} />
-            <meta name="twitter:image" content={`${process.env.REACT_APP_ENVIROMENT}/api/og?title=$Magia&image=wand`}></meta>
+            <meta property="og:image" content={`${process.env.REACT_APP_ENVIROMENT}/api/og?title=${spellName}&image=wand`} />
+            <meta name="twitter:image" content={`${process.env.REACT_APP_ENVIROMENT}/api/og?title=${spellName}&image=wand`}></meta>
         </Head>
         <div className="main-header">
           <h1>Magias</h1>
@@ -88,4 +91,31 @@ export default function SpellsName({AllSpells}: any) {
         </div>
     </Container>
   );
+}
+
+type routes ={
+  params:{
+      spellName: string
+  }
+}
+
+export async function getStaticProps(name: routes) {
+
+    const res = await fetch(`https://rpg-73e66-default-rtdb.firebaseio.com/spells.json?orderBy="SpellName"&equalTo="${name.params.spellName}"`)
+    const AllSpells: SpellsProps[] = await res.json();
+
+    return{
+      props:{
+      AllSpells,
+      fallback: false
+      }
+    }
+}
+
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+
+  return {
+      paths: [], //indicates that no page needs be created at build time
+      fallback: 'blocking' //indicates the type of fallback
+  }
 }
