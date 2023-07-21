7b4b5b93c1cacc749c91c16767581a0a25ffbfcd
juan-20 <juanalvesandrade@gmail.com>
2023-07-21 19:21:32 -0300

    adding login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import CharacterCard from "~/components/CharacterCard";
 import { api } from "~/utils/api";
 
 export default function Home() {
+  const { data: sessionData } = useSession();
+
 
   return (
     <>
@@ -15,7 +17,12 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-
+        <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
          
             <CharacterShow />
         </div>
