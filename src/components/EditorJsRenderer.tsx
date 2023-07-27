6ac55f8cbe4c3bfd2353b7cf7367a4c3b28bd40c
juan-20 <juanalvesandrade@gmail.com>
2023-07-27 00:14:59 -0300

    a

--- src/components/EditorJsRenderer.tsx
+++ src/components/EditorJsRenderer.tsx
@@ -1,38 +1,38 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import {
- BlockToolData,
- type OutputBlockData,
- type OutputData,
-} from '@editorjs/editorjs'
-import React from 'react'
-const editorJsHtml = require('editorjs-html')
+// /* eslint-disable @typescript-eslint/no-var-requires */
+// /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// /* eslint-disable @typescript-eslint/no-unsafe-call */
+// import {
+//  BlockToolData,
+//  type OutputBlockData,
+//  type OutputData,
+// } from '@editorjs/editorjs'
+// import React from 'react'
+// const editorJsHtml = require('editorjs-html')
 
-const EditorJsToHtml = editorJsHtml({
- code: (block: OutputBlockData<string>) => {
-  console.log(block)
-  return <CodeRenderer code={block.data.code} />
- },
-})
+// const EditorJsToHtml = editorJsHtml({
+//  code: (block: OutputBlockData<string>) => {
+//   console.log(block)
+//   return <CodeRenderer code={block.data.code} />
+//  },
+// })
 
-interface Props {
- data: OutputData
-}
-type ParsedContent = string | JSX.Element
+// interface Props {
+//  data: OutputData
+// }
+// type ParsedContent = string | JSX.Element
 
-const EditorJsRenderer = ({ data }: Props) => {
- const html = EditorJsToHtml.parse(data) as ParsedContent[]
- return (
-  <div className="prose max-w-full ">
-   {html.map((item, index) => {
-    if (typeof item === 'string') {
-     return <div dangerouslySetInnerHTML={{ __html: item }} key={index}></div>
-    }
-    return item
-   })}
-  </div>
- )
-}
+// const EditorJsRenderer = ({ data }: Props) => {
+//  const html = EditorJsToHtml.parse(data) as ParsedContent[]
+//  return (
+//   <div className="prose max-w-full ">
+//    {html.map((item, index) => {
+//     if (typeof item === 'string') {
+//      return <div dangerouslySetInnerHTML={{ __html: item }} key={index}></div>
+//     }
+//     return item
+//    })}
+//   </div>
+//  )
+// }
 
-export default EditorJsRenderer
+// export default EditorJsRenderer
